8e39ab3a1a8a6be19616cfcb07d605844975b485
lk23286 <lk23286@gmail.com>
2016-02-21 10:58:24 +0100

    started

--- gitFun2/ViewController.swift
+++ gitFun2/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Git excesise has started
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
