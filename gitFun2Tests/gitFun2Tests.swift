bc7692caec32f3545873cf9ddec66e232a6e1264
lk23286 <lk23286@gmail.com>
2016-02-21 10:52:01 +0100

    Initial Commit

---
+++ gitFun2Tests/gitFun2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  gitFun2Tests.swift
+//  gitFun2Tests
+//
+//  Created by lk23286 on 2016. 02. 21..
+//  Copyright © 2016. lk23286. All rights reserved.
+//
+
+import XCTest
+@testable import gitFun2
+
+class gitFun2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
